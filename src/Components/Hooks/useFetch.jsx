eaa2c3672cda3b8838e0d68e44c860e35d599364
ewi josepha <ewijosepha@gmail.com>
2023-11-28 21:59:21 +0100

    adding values in local storagee

--- src/Components/Hooks/useFetch.jsx
+++ src/Components/Hooks/useFetch.jsx
@@ -1,16 +1,27 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { useState } from "react";
+
 function useFetch() {
+  const [postperpage, setPostperpage]= useState(1)
+  const [currentpage, setCurrentpage]= useState(1)
+  const [amount, setAmount]= useState([])
+  // const amount = 1
   // const navigate = useNavigate();
   const { data, isLoading, error } = useQuery({
     queryKey: ["quizz"],
     queryFn: async () => {
       const res = await axios.get(
-        "https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean"
+        `https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean`
       );
       return res.data.results
     },
   });
+
+  const indexoflastpage = currentpage * postperpage
+  const indexoffirstpost = indexoflastpage - postperpage
+  // const currentpost = data.slice(indexoflastpage, indexoffirstpost);
+
 
   // console.log(data);
   return {data,isLoading,error}
--- src/Components/Quiz/Question.jsx
+++ src/Components/Quiz/Question.jsx
@@ -1,36 +1,90 @@
 import useFetch from "../Hooks/useFetch";
-import { useEffect } from "react";
-import "../Pages/pages.css"
+import { useEffect, useState } from "react";
+import useLocalStorage from "use-local-storage";
 
+import "../Pages/pages.css";
 
 function Questions() {
-  const {data, isLoading,error}= useFetch()
-  if(isLoading){
-    <h1>Loading</h1>
+  const [truee, setTruee]= useState()
+  const [falsee, setFalsee]= useState()
+  const array= []
+
+
+  let { data, isLoading, error } = useFetch();
+  if (isLoading) {
+    <h1>Loading</h1>;
   }
 
-  if(error){
-    <h1>An error occured</h1>
+  if (error) {
+    <h1>An error occured</h1>;
   }
 
- console.log(data);
-  return (
+  function handletrue () {
+  setTruee(true);
+  }
+
+  function handlefalse (quest) {
+  setFalsee(false);
+  const falseval = JSON.stringify(localStorage.getItem("anser") || [])
+  const correctAns = quest.correct_answer
+  if(correctAns){
+    const rightans = falsee.filter((rightans) => rightans === falsee); // removing meal from localstorage
+
+    array.push(correctAns)
+    localStorage.setItem("anser", JSON.stringify(array));
+    console.log(rightans);
+
+  }else{
+    const updat=[...falseval, correctAns]
+    localStorage.setItem("anser", JSON.stringify(updat));
+
+  }
+//   const prevans = falseval.find((ans)=>{
+//     ans === correctAns 
+// })
+
+ 
+
+
+
+
+  console.log(correctAns);
+
+  }
+
+  // const trueval = JSON.stringify(localStorage.setItem("anser", truee))
+  
+
+
+    // console.log(truee);
+    // console.log(falsee);
+    console.log(data);
+
+
+    return (
     <>
-    <div className="ans">
-      <h2 id="title">Click on either True or False to choose the correct answer</h2>
-   
-    <div className="quest">
-    <h4 id="quizz">{data[0]?.question}</h4>
+      <div className="ans">
+        <h2 id="title">
+          Click on either <span>True</span> or <span>False</span> to choose the
+          correct answer
+        </h2>
+        <hr />
+        {data?.map((quest) => (
+          <>
+            <div className="quest">
+              <h4 id="quizz">{quest.question}</h4>
+            </div>
+            <div className="choice">
+              <button id="tru" onClick={handletrue}>True</button>
+              <button id="fals" onClick={()=>handlefalse(quest)}>False</button>
+            </div>
+          </>
+        ))}
 
-    </div>
-    <div className="choice">
-      <button id="tru">True</button>
-      <button id="fals">False</button>
-    </div>
-    <div className="submit">
-      <button id="submit">Submit</button>
-    </div>
-    </div>
+        <div className="submit">
+          <button id="submit">Submit</button>
+        </div>
+      </div>
     </>
   );
 }
